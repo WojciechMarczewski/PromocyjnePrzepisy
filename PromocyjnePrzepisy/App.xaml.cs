5e9481b1c694d42b849fb85c15d91112886fd45c
WojtusiakIsCoding <wojciech.marczewski@interia.pl>
2024-04-18 21:59:40 +0200

    Init

---
+++ PromocyjnePrzepisy/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PromocyjnePrzepisy
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PromocyjnePrzepisy/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PromocyjnePrzepisy
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
