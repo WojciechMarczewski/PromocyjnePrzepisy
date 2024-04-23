ee68cbed1336f0622f289502ab781c066a348b39
WojtusiakIsCoding <wojciech.marczewski@interia.pl>
2024-04-24 01:02:44 +0200

    RecipePage implementation.

--- PromocyjnePrzepisy/App.xaml.cs
+++ PromocyjnePrzepisy/App.xaml.cs
@@ -7,6 +7,9 @@
             InitializeComponent();
 
             MainPage = new AppShell();
+            //Temporarily navigate to currently created page at app start
+            Task.Run(async () => await Shell.Current.GoToAsync("//RecipePage"));
+
         }
     }
 }
