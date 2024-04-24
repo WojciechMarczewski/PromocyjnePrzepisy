926ba4b965cd354d8fbe7108f3875c99db8c1076
WojtusiakIsCoding <wojciech.marczewski@interia.pl>
2024-04-25 01:14:57 +0200

    MainPage Layout updates.

---
+++ PromocyjnePrzepisy/viewModels/CollectionViewItem.cs
@@ -0,0 +1,9 @@
+﻿namespace PromocyjnePrzepisy.ViewModels
+{
+    class CollectionViewItem
+    {
+        public string? Text { get; set; }
+        public ImageSource? Source { get; set; }
+        public string? DiscountsCount { get; set; }
+    }
+}
